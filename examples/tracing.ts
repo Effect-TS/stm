787ffd50c5ce9c36d3079af7c0f8ec62ee195214
Tim <hello@timsmart.co>
2023-07-18 14:25:18 +1200

    update /io (#106)

--- examples/tracing.ts
+++ examples/tracing.ts
@@ -6,4 +6,4 @@ const transaction = STM.flatMap(
   (n) => STM.fail(new Error(`n: ${n}`))
 )
 
-Effect.runFork(Effect.catchAllCause(transaction, Effect.logCause("Error")))
+Effect.runFork(Effect.catchAllCause(transaction, Effect.logError))
