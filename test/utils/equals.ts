1bb8f489e77c851cb17cdf899cea2ae0ed1245fc
Tim <hello@timsmart.co>
2023-08-04 10:55:48 +1200

    update build tools (#115)

--- test/utils/equals.ts
+++ test/utils/equals.ts
@@ -1,12 +1,11 @@
 import * as Equal from "@effect/data/Equal"
 import type * as Equivalence from "@effect/data/Equivalence"
 
-export const equivalentElements = <A>(): Equivalence.Equivalence<A> =>
-  (x, y) => {
-    if (Array.isArray(x) && Array.isArray(y)) {
-      if (x.length === y.length) {
-        return x.every((v, i) => Equal.equals(v, y[i]))
-      }
+export const equivalentElements = <A>(): Equivalence.Equivalence<A> => (x, y) => {
+  if (Array.isArray(x) && Array.isArray(y)) {
+    if (x.length === y.length) {
+      return x.every((v, i) => Equal.equals(v, y[i]))
     }
-    return Equal.equals(x, y)
   }
+  return Equal.equals(x, y)
+}
